037c8d306931d6f2929fde4d3a9028fe9c0e0e49
zarj369-developer <zarj369963@gmail.com>
2026-09-24 05:19:49 -1200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+
+var Buut = document.querySelectorAll(".drum") ;
+    
+
+console.log(Buut);
+for(var i=0 ;i<Buut.length ;i++){
+    Buut[i].addEventListener("click",function (){
+         var buttonLetter = this.innerHTML;
+
+        switch (buttonLetter) {
+
+            case "w":
+                var audio = new Audio("sounds/crash.mp3");
+                audio.play();
+                break;
+
+            case "a":
+                var audio = new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;
+
+             case "s":
+                var audio = new Audio("sounds/snare.mp3");
+                audio.play();
+                break;
+            case "d":
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+            case "j":
+                var audio = new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case "k":
+                var audio = new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;
+            case "l":
+                var audio = new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break;
+
+        }
+});
+};
